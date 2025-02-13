1f01bfe6bb626cd0facd2e1ce32d2c02e4613215
BandaySajid <mbandaysajid@gmail.com>
2025-02-13 15:52:50 +0530

    fix returing after sending response

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Request, Response } from 'express';
 import Redis from "ioredis";
 import { deleteHostData } from './util.js';
 import { REDIS_URL, COMMUNICATOR_PORT, COMMUNICATOR_SECRET } from './config.js';
@@ -10,10 +9,11 @@ const server = express();
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
-server.delete('/cache/hosts/:id', async (req: Request, res: Response) => {
+server.delete('/cache/hosts/:id', async (req: express.Request, res: express.Response): Promise<void> => {
   const { id } = req.params;
   if (req.headers.authorization !== COMMUNICATOR_SECRET) {
     res.status(401).send({ success: false, error: "Unauthorized" });
+    return
   }
   try {
     await deleteHostData(redis, id);
@@ -23,7 +23,6 @@ server.delete('/cache/hosts/:id', async (req: Request, res: Response) => {
     res.status(500).send({ success: false, error: "Error deleting host data cache" });
   }
 });
-
 export default function run() {
   server.listen(COMMUNICATOR_PORT, '127.0.0.1', () => {
     console.log(`Communication service listening on port`, COMMUNICATOR_PORT);
